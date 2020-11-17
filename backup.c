27b7fb73a1205c98d827a79035897b2686ece9a7
angelchavez013 <chavez@Chavez-HP.localdomain>
2020-11-16 18:53:30 -0600

    Creo que ya esta terminado el programa, solo hay pequeños detalles

--- backup.c
+++ backup.c
@@ -3,28 +3,27 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <time.h>
 
 #define SIZE 512
 
 int main(){
-	FILE *file;
-	int pfd[2],pfd2[2],pfd3[2];
+	FILE * file;
+	int pfd[2],pfd2[2],pfd3[2],pfd4[2];
 	char msg[SIZE], linea[SIZE];
 	char buffer[SIZE];
 	char dirR[60];
-	char dirW[60], *dirW2;
+	char dirW[60];
 	char command[70];
+	char lineas[40];
 	pid_t pid;
-	int status, lineanum, contador;
-	
+	int status, lineanum=0, contador=0, total;
+
 	system ("clear");
-	// Se reciben las rutas d eorigen del respaldo y destino
+
 	printf("Ingresa la ruta absoluta del directorio a respaldar: ");
 	scanf("%s", dirR);
 	printf("Ingresa la ruta absoluta del directorio destino: ");
 	scanf("%s", dirW);
-	//printf("%s, %s\n", dirR, dirW);
 
 	// El comando test realiza una verificación segun la opcion señalada sobre un directorio o archivo
 	// en este caso regresa 0 si existe el directorio indicado
@@ -35,7 +34,6 @@ int main(){
 		printf("No existe el directorio indicado. No se puede realizar un respaldo.\n");
 		exit(0);
 	}
-
     printf("Padre: %i", getpid());
     printf("\n=======================================\n");
     printf("\tGENERANDO LISTA DE ARCHIVOS A RESPALDAR\n");
@@ -44,10 +42,10 @@ int main(){
 	strcat(command,"ls ");
 	strcat(command,dirR);
 	strcat(command," >> test.txt");
+	//system("ls -l tareasSO | wc -l >> test.txt"); //Guardar la informacion de un comando
 	system(command);
-		
+
 	// Crea el directorio de respaldo, si existe lo elimina y crea uno limpio
-	//dirW2 = "%s $(date +%d%m%Y_%H_%M_%S)",dirW;
 	strcpy(command,"");
 	strcat(command,"test -d ");
     strcat(command,dirW);
@@ -55,22 +53,20 @@ int main(){
 		strcpy(command,"");
 		strcat(command,"mkdir ");
 		strcat(command,dirW);
-		//strcat(command,"_$(date +%d%m%Y_%H_%M_%S)");
 		system(command);
 	}else{
+		printf("Eliminando respaldo anterior...\n");
 		strcpy(command,"");
-		strcat(command,"rm -r ");
+		strcat(command,"rm -r -v ");
 		strcat(command,dirW);
-		//strcat(command," *");
 		system(command);
 		strcpy(command,"");
 		strcat(command,"mkdir ");
 		strcat(command,dirW); //Aquí concatenar la fecha y hora
-		//strcat(command,"_$(date +%d%m%Y_%H_%M_%S)"); 
+		//strcat(command,"_$(date +%d%m%Y_%H_%M_%S)");
 		system(command);
 	}
 	// Hasta aquí genera la lista de archivos y crea o sobreescribe el directorio respaldo
-
 
 	if (pipe(pfd) < 0) { 
 		perror("\nError al crear el pipe");
@@ -79,6 +75,9 @@ int main(){
         perror("\nError al crear el pipe");
 		exit(1);
     }else if(pipe(pfd3) < 0){
+        perror("\nError al crear el pipe");
+		exit(1);
+    }else if(pipe(pfd4) < 0){
         perror("\nError al crear el pipe");
 		exit(1);
     }
@@ -91,39 +90,54 @@ int main(){
         break;
 
         case 0:
-          	close(pfd[1]); // Cierra el descriptor de escritura que no va a usar.
+          	close(pfd[1]);
 			close(pfd2[0]);
 			close(pfd3[1]);
+			close(pfd4[0]);
 
      		printf("\nHijo(pid=%i) esperando mensaje de mi padre...\n", getpid());
 			read(pfd[0],msg,SIZE);
 			printf("\nHijo(pid=%i), instruccion del padre: %s\n", getpid(), msg);
 			
 			write(pfd2[1],"<--- Hola padre, ¿de cuantos archivos sera el respaldo?...",SIZE);
-			read(pfd3[0],msg,SIZE);
 			
-			while(read(pfd3[0],msg,SIZE)){
-				if (msg == "FIN")
+			read(pfd3[0],msg,SIZE);
+			total = atoi(msg);
+			//read(pfd3[0],msg,SIZE)
+			contador = total;
+			while(contador > 0){
+				read(pfd3[0],msg,SIZE);
+				if (msg == "FIN"){
 					break;
-				strcpy(command,"");
-				strcat(command,"cp ");
-				strcat(command,msg);
-				strcat(command," ");
-				strcat(command,dirW);
-				printf("%s\n", command);
+				}else{
+					printf("\tRealizando el respaldo de %s\tArchivos pendientes: %d/%d\n", msg,contador--,total);
+					strcpy(command,"");
+					strcat(command,"cp ");
+					strcat(command,dirR); 
+					strcat(command,"/");
+					strcat(command,msg);
+					strcat(command," ");
+					strcat(command,dirW);
+					//printf("%s\n", command);
+					system(command);
+				}
 			}
-			printf("Archivos leidos con exito\n");
+			printf("\nArchivos leidos con exito\n"); // ENVÍAR MENSAJE AL PADRE
 
-			close(pfd[0]); //Cierra su canal de lectura porque ya termino
+			write(pfd4[1],"Termine de hacer el respaldo", SIZE);
+
+			close(pfd[0]);
 			close(pfd2[1]);
 			close(pfd3[0]);
+			close(pfd4[1]);
 			exit(0);
         break;
 
         default:
-          	close(pfd[0]); // Cierra el descriptor de lectura que no va a usar.
+          	close(pfd[0]);
 			close(pfd2[1]);
 			close(pfd3[0]);
+			close(pfd4[1]);
 
 			write(pfd[1],"Realiza el siguiente respaldo",SIZE);
 
@@ -131,7 +145,6 @@ int main(){
 			printf("\nPadre(pid=%i), lee mensaje del hijo: %s\n", getpid(), buffer);
 			
 			file = fopen("test.txt","r");
-
 			if(file == NULL){
 				perror("\nError al abrir el archivo");
 			}else{
@@ -141,25 +154,46 @@ int main(){
 					fscanf(file,"%s ",palabra);
 					lineanum++;
 				}
-				printf("lineanum: %d\n", lineanum);
+				printf("\nNumero de archivos a respaldar: %d\n", lineanum);
 				//Rebobina el apuntador para volver a leer el archivo
 				rewind(file);
+				sprintf(lineas,"%d",lineanum);
+				write(pfd3[1],lineas,SIZE);
 				for (int i = 0; i < lineanum; ++i){
 					fgets (linea, sizeof linea, file);
 					strtok(linea,"\n");
 					write(pfd3[1],linea,SIZE);
 				}
-				write(pfd3[1],"FIN",SIZE);
-				
 			}
+
+			write(pfd3[1],"FIN",SIZE);
+			//(Validar que haya terminado el hijo)
 			if(fclose(file) != 0){
 				printf("Error al cerrar el directorio de respaldo.\n");
 				exit(0);
 			}
+			
+			read(pfd4[0], buffer, SIZE);
 
-			close(pfd[1]); //Cierra su canal de escritura porque ya termino
+			printf("\nPadre(pid=%i), lee mensaje del hijo: %s\n", getpid(),buffer);
+			printf("\nPadre(pid=%i), comprobando respaldo:\n", getpid());
+			//Lista el directorio de respaldo (Validar que haya terminado el hijo)
+			strcpy(command,"");
+			strcat(command,"ls -l ");
+			strcat(command,dirW);
+			system(command);
+			strcpy(command,"");
+			strcat(command,"ls -l ");
+			strcat(command,dirW);
+			strcat(command," | wc -l");
+			system(command);
+			printf("ARCHIVOS RESPALDADOS\n");
+			printf("========================================================================\n");
+			
+			close(pfd[1]);
 			close(pfd2[0]);
 			close(pfd3[1]);
+			close(pfd4[0]);
 		break;
 	}
 	wait(&status);
